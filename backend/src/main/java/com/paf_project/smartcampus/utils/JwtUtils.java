96e9911ec6473fd731b09ed480481c69ed3d9321
uvindu abhisheka <dharmasenaua@gmail.com>
2026-03-05 03:10:15 +0530

    Implement User model, repository, service, and JWT utility for authentication

---
+++ backend/src/main/java/com/paf_project/smartcampus/utils/JwtUtils.java
@@ -0,0 +1,33 @@
+package com.paf_project.smartcampus.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+
+
