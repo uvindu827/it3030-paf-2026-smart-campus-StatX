0baf24a5f9a3e391cd98b49d2d5d8011a0752612
Indhi <indhiwanithima123@gmail.com>
2026-04-11 13:51:16 +0530

    feat: add react-router-dom for routing and update App component with routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import HotelBooking from './features/HotelBooking/hotelbooking';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HotelBooking from "./features/HotelBooking/hotelbooking";
+import ResourcesPage from "./features/resources/ResourcesPage";
+// import "./App.css";
 
 function App() {
   useEffect(() => {
     fetch("http://localhost:8080")
-      .then(res => res.text())
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+      .then((res) => {
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        return res.text();
+      })
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
-    <div className="App">
-      <HotelBooking />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HotelBooking />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
