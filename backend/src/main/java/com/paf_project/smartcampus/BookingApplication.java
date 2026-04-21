817e612c270b30fcb58e5b783cc0e11a51b3030b
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2026-04-21 11:47:15 +0530

    feat: add main method to bootstrap Spring Boot application

--- backend/src/main/java/com/paf_project/smartcampus/BookingApplication.java
+++ backend/src/main/java/com/paf_project/smartcampus/BookingApplication.java
@@ -9,4 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
 }
