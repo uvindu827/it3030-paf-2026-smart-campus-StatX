e06667d151ef69d4e240ae1c1613259c3b94d1e1
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2026-04-21 11:46:59 +0530

    feat: create main Spring Boot application class

--- backend/src/main/java/com/paf_project/smartcampus/BookingApplication.java
+++ backend/src/main/java/com/paf_project/smartcampus/BookingApplication.java
@@ -9,7 +9,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingApplication.class, args);
-	}
 }
